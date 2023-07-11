adc8c7e3a9df3f4e6944bace250304cd59000f9c
isabelle_lemire_web <isabellelemire36@gmail.com>
2023-07-10 22:02:14 -0400

    enlever maskable

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v19';
+const CACHE_NAME = 'static-cache-v20';
 console.log('coucou');
 
 //Add list of files to cache here.
