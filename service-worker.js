fe6af3c45e8ef6dee6b21228bce613b7b5755ffb
isabelle_lemire_web <isabellelemire36@gmail.com>
2023-07-10 20:37:50 -0400

    2

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 //Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v8';
+const CACHE_NAME = 'static-cache-v9';
 console.log('coucou');
 
 //Add list of files to cache here.
